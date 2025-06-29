1e56542b9dc4d5c70f2775f7fb15cfc23ba2d32f
JeremyDuncan <jduncan46@my.devry.edu>
2025-06-29 19:43:24 -0400

    updated Image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,14 @@
 class Image < ApplicationRecord
+  belongs_to :imageable, polymorphic: true, inverse_of: :images
   belongs_to :user
-  belongs_to :imageable, polymorphic: true
+
+  has_one_attached :file
+
+  before_validation :assign_current_user
+
+
+  private
+    def assign_current_user
+      self.user ||= Current.user
+    end
 end
